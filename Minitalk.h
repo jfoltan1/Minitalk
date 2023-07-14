61f966b9310e39ee502d87532f3a6421b115cf0d
Julius Foltan <jfoltan@4c0c1.42berlin.de>
2023-07-14 12:43:45 +0200

    new version with signal verification

---
+++ Minitalk.h
@@ -0,0 +1,11 @@
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# define _GNU_SOURCE
+
+# include <signal.h>
+# include <sys/types.h>
+# include <unistd.h>
+# include "./libft/libft.h"
+
+#endif
---
+++ libft/ft_strrchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfoltan <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/02 19:02:10 by jfoltan           #+#    #+#             */
+/*   Updated: 2022/12/02 19:02:11 by jfoltan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+#include <stddef.h>
+#include <string.h>
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	while (i >= 0)
+	{
+		if (s[i] == (char)c)
+			return ((char *)(s + i));
+		i--;
+	}
+	return (NULL);
+}
